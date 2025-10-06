77bc493bb870124e2e4acb16beeb720e88e06d24
Diego Abanto <dgabanto27@gmail.com>
2025-10-05 20:03:17 -0400

    Add files

---
+++ level-1-current/bullet_0.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+var pos:Vector2
+var rot:float
+var dir:float
+
+var speed = 2000
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	global_position = pos
+	global_rotation = rot
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	velocity = Vector2(speed, 0).rotated(dir)
+	move_and_slide()
---
+++ level-1-current/character_body_2d.gd
@@ -0,0 +1,51 @@
+extends CharacterBody2D
+
+var bullet_path = preload("res://bullet_0.tscn")
+
+const SPEED = 450.0
+var idle_dir:String
+
+func _physics_process(delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.	
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	look_at(get_global_mouse_position()) # Current position of mouse, may use
+	
+	velocity = direction * SPEED
+	
+	
+	if direction.y == -1.0:
+		$AnimatedSprite2D.play("move_up")
+		idle_dir = "idle_up"
+	elif direction.y == 1.0:
+		$AnimatedSprite2D.play("move_down")
+		idle_dir = "idle_down"
+	elif direction.x > 0.7: #Takes care of right diagonals too
+		$AnimatedSprite2D.play("move_right")
+		idle_dir = "idle_right"
+	elif direction.x < -0.7: #Takes care of left diagonals
+		$AnimatedSprite2D.play("move_left")
+		idle_dir = "idle_left"
+	elif direction == Vector2.ZERO:
+		$AnimatedSprite2D.play(idle_dir)
+	else:
+		$AnimatedSprite2D.play(idle_dir)
+	
+	move_and_slide()
+	
+func _input(ev):
+	if ev is InputEventMouseButton: #Mouse clicks
+		# Mouse 1 = L. Click; Mouse 2 = R. Click
+		# Mouse 3 = Wheel click, Mouse 4 = Scroll up; Mouse 5 = Scroll down
+		print(ev)
+		if ev.button_index == 1 and ev.button_mask == 1: # button mask 1 is click down, 0 is up
+			var bullet = bullet_path.instantiate() # Get bullet instance
+			bullet.dir = rotation
+			bullet.pos = $Bullet_Pos.global_position
+			bullet.rot = global_rotation
+			get_parent().add_child(bullet)
+			
+			print(get_global_mouse_position())
+	
+	
