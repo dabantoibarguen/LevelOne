b5a033782d4c5e51b3c8ecc39df64ef9d3d656e7
dabantoibarguen <dgabanto27@gmail.com>
2025-10-12 12:37:09 -0400

    Self deleting bullets

--- level-1-current/bullet_1.gd
+++ level-1-current/bullet_1.gd
@@ -2,17 +2,36 @@ extends Area2D
 var pos:Vector2
 var rot:float
 var dir:float
-var jose_id:int
 
-var speed = 2000
+var speed = 20
+
+var offscreen_timer := 0.0
+var offscreen = false
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	global_position = pos
 	global_rotation = rot
-
+	body_entered.connect(_on_body_entered)
+	$VisibleNotif.screen_exited.connect(_on_screen_exit)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	position += (Vector2(speed, 0).rotated(dir))/50
+	position += (Vector2(speed, 0).rotated(dir))
+	if offscreen:
+		offscreen_timer += delta
+		if offscreen_timer >= 2.0:
+			print("GOONERw")
+			queue_free()
+		
+
+func _on_body_entered(body):
+	if body.name != "jose":
+		visible = false
+		queue_free()
+	
+func _on_screen_exit():
+	print("GONE")
+	offscreen = true
 	
