c280942d7a34cc51c86f1c9be8da7cacd5cce6b5
Diego Abanto <dabantoibarguen@colgate.edu>
2026-09-08 21:21:35 -0500

    Broken Swing file

--- level-1-current/scenes/weapons/swing_0.gd
+++ level-1-current/scenes/weapons/swing_0.gd
@@ -4,7 +4,7 @@ var parent:CharacterBody2D
 var dir:Vector2
 var dmg = 1
 
-@onready var origin_category = get_parent().category
+var origin_category:String
 
 
 func play_anim():
@@ -27,7 +27,7 @@ func _physics_process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D):
 	if body is CharacterBody2D:
-		if visible and body.category != origin_category:
+		if visible and body.category != "Enemy":
 			#var parent = get_parent()
 			body.take_damage(dmg)
 		
